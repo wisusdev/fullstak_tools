e8b8c7fc3e4c9a5fd3d34c483881e53488d0d844
Jesus Avelar <wisusdev@gmail.com>
2024-11-13 09:10:25 -0600

    Actualiza el menú lateral para mostrar el título basado en el sistema operativo y añade opciones de navegación para Terminal y SSH.

--- lib/app/widgets/side_menu_drawer.dart
+++ lib/app/widgets/side_menu_drawer.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class SideMenuDrawer extends StatelessWidget {
@@ -5,6 +7,17 @@ class SideMenuDrawer extends StatelessWidget {
 
     @override
     Widget build(BuildContext context) {
+
+        String homeTitle = 'Servicios';
+
+        if (Platform.isMacOS) {
+            homeTitle = 'MAMP';
+        } else if (Platform.isLinux) {
+            homeTitle = 'LAMP';
+        } else if (Platform.isWindows) {
+            homeTitle = 'WAMP';
+        }
+
         // Drawer es un widget que muestra un panel lateral que normalmente se usa para proporcionar navegación a través de la aplicación.
         return Drawer(
             // ListView crea un scrollable, linear array de widgets.
@@ -21,18 +34,32 @@ class SideMenuDrawer extends StatelessWidget {
                             color: Colors.blue,
                         ),
                         // child es un widget que se muestra en el DrawerHeader. En este caso, es un Text. 
-                        child: Text('Menú', style: TextStyle(color: Colors.white)),
+                        child: Text('Avegonia-AIO', style: TextStyle(color: Colors.white)),
                     ),
                     // ListTile es un widget que muestra un elemento en un ListView.
                     ListTile(
                         // title es un widget que se muestra en la parte superior de un ListTile.
-                        title: const Text('Inicio'),
+                        title: Text(homeTitle),
                         // onTap es una devolución de llamada que se llama cuando el usuario toca el ListTile.
                         onTap: () {
                             // Navigator.pop es una función que elimina la ruta actual de la pila de rutas y navega a la ruta anterior.
+                            Navigator.pushNamed(context, '/home');
+                        },
+                    ),
+                    ListTile(
+                        title: const Text('Terminal'),
+                        onTap: () {
+                            Navigator.pushNamed(context, '/terminal');
+                        },
+                    ),
+
+                    ListTile(
+                        title: const Text('SSH'),
+                        onTap: () {
                             Navigator.pop(context);
                         },
                     ),
+
                     ListTile(
                         title: const Text('Configuración'),
                         onTap: () {
