0a1a811b81f9848da765bd8b2b6bf2eac27c4e69
Jesus Avelar <wisusdev@gmail.com>
2024-11-13 00:15:48 -0600

    Añade la clase PHPService para gestionar la instalación y obtención de la versión de PHP en Windows.

---
+++ lib/app/platforms/windows/php_service.dart
@@ -0,0 +1,83 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class PHPService {
+
+    static Future<String> getVersion() async {
+        var process = await Process.start('php', ['--version'], runInShell: true);
+
+        await process.stdin.close();
+
+        var output = await process.stdout.transform(const SystemEncoding().decoder).join();
+        var error = await process.stderr.transform(const SystemEncoding().decoder).join();
+
+        var versionMatch = RegExp(r'PHP (\d+\.\d+\.\d+)').firstMatch(output);
+        
+        if (versionMatch != null) {
+            return versionMatch.group(1) ?? '';
+        }
+
+        if (error.isNotEmpty) {
+            print('Error: $error');
+            return '';
+        }
+
+        return output;
+    }
+
+    static Future<void> install() async {
+
+        print('Instalando PHP...');
+
+        var process = await Process.start('choco', ['install', 'php'], runInShell: true);
+
+        await process.stdin.close();
+
+        var output = await process.stdout.transform(const SystemEncoding().decoder).join();
+        var error = await process.stderr.transform(const SystemEncoding().decoder).join();
+
+        if (error.isNotEmpty) {
+            print('Error: $error');
+        }
+
+        print(output);
+    }
+
+    static Future<void> uninstall() async {
+
+        print('Desinstalando PHP...');
+
+        var process = await Process.start('choco', ['uninstall', 'php'], runInShell: true);
+
+        await process.stdin.close();
+
+        var output = await process.stdout.transform(const SystemEncoding().decoder).join();
+        var error = await process.stderr.transform(const SystemEncoding().decoder).join();
+
+        if (error.isNotEmpty) {
+            print('Error: $error');
+        }
+
+        print(output);
+    }
+
+    static List<Widget> actions(BuildContext context) {
+        return [
+            IconButton(
+                icon: const Icon(Icons.download),
+                color: Colors.white,
+                onPressed: () {
+                    install();
+                },
+            ),
+            IconButton(
+                icon: const Icon(Icons.delete),
+                color: Colors.white,
+                onPressed: () {
+                    uninstall();
+                },
+            ),
+        ];
+    }
+}
