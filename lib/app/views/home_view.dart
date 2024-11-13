73a13b8d1ec3ee5aa19e662afe3a2ce95dbd37a9
Jesus Avelar <wisusdev@gmail.com>
2024-11-13 09:10:01 -0600

    Elimina el uso del terminal y la lógica asociada en HomeView; simplifica la interfaz mostrando un texto estático en su lugar.

--- lib/app/views/home_view.dart
+++ lib/app/views/home_view.dart
@@ -1,15 +1,10 @@
-import 'dart:convert';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_pty/flutter_pty.dart';
 import 'package:fullstak_tools/app/platforms/windows/services.dart';
 import 'package:fullstak_tools/app/shared/operating_system.dart';
 import 'package:fullstak_tools/app/widgets/list_tile_service.dart';
 import 'package:fullstak_tools/app/widgets/side_menu_drawer.dart';
 import 'package:fullstak_tools/app/shared/responsive_layout.dart';
-import 'package:xterm/xterm.dart';
 
 class HomeView extends StatefulWidget {
 	const HomeView({super.key});
@@ -28,15 +23,10 @@ class _HomeViewState extends State<HomeView> {
 		WindowsService(name: 'MySQL', version: 'unknown', icon: Icons.storage, color: Colors.grey),
 	];
 
-    late final Pty pty;
-
-    final terminal = Terminal(
-        maxLines: 10000,
-    );
-
 	@override
 	void initState() {
 		super.initState();
+
         _loadPlatformInfo();
 
 		for (var service in services) {
@@ -52,35 +42,7 @@ class _HomeViewState extends State<HomeView> {
 				});
 			}
 		}
-
-        WidgetsBinding.instance.endOfFrame.then(
-            (_) {
-                if (mounted) _startPty();
-            },
-        );
 	}
-
-    void _startPty() {
-        pty = Pty.start(
-            shell,
-            columns: terminal.viewWidth,
-            rows: terminal.viewHeight,
-        );
-
-        pty.output.cast<List<int>>().transform(const Utf8Decoder()).listen(terminal.write);
-
-        pty.exitCode.then((code) {
-            terminal.write('the process exited with exit code $code');
-        });
-
-        terminal.onOutput = (data) {
-            pty.write(const Utf8Encoder().convert(data));
-        };
-
-        terminal.onResize = (w, h, pw, ph) {
-            pty.resize(h, w);
-        };
-    }
 
     Future<void> _loadPlatformInfo() async {
         String version = await OperatingSystem().getPlatformInfo();
@@ -89,8 +51,6 @@ class _HomeViewState extends State<HomeView> {
             sistemVersion = version;
         });
     }
-
-    final terminalController = TerminalController();
 
   	@override
   	Widget build(BuildContext context) {
@@ -130,35 +90,14 @@ class _HomeViewState extends State<HomeView> {
                         decoration: const BoxDecoration(
                             color: Colors.black,
                         ),
-						child: TerminalView(
-                            terminal,
-                            controller: terminalController,
-                            autofocus: true,
-                            backgroundOpacity: 0.7,
-                            onSecondaryTapDown: (details, offset) async {
-                                final selection = terminalController.selection;
-                                
-                                if (selection != null) {
-                                    final text = terminal.buffer.getText(selection);
-                                    terminalController.clearSelection();
-                                    await Clipboard.setData(ClipboardData(text: text));
-                                } else {
-                                    final data = await Clipboard.getData('text/plain');
-                                    final text = data?.text;
-                                    
-                                    if (text != null) {
-                                        terminal.paste(text);
-                                    }
-                                }
-                            },
-                        ),
+						child: const Text('Terminal', style: TextStyle(color: Colors.white)),
 					);
 
 
 
 					List<Widget> children = <Widget>[
 						runProcess,
-                        Responsive.isMobile(context) ? Expanded(child: infoProcess) : infoProcess,
+                        infoProcess,
 					];
 
 					return Responsive.isMobile(context) ? Column(children: children) : Row(children: children);
@@ -168,16 +107,4 @@ class _HomeViewState extends State<HomeView> {
       		drawer: const SideMenuDrawer(),
     	);
   	}
-}
-
-String get shell {
-    if (Platform.isMacOS || Platform.isLinux) {
-        return Platform.environment['SHELL'] ?? 'bash';
-    }
-
-    if (Platform.isWindows) {
-        return 'cmd.exe';
-    }
-
-    return 'sh';
 }
