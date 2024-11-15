00ff65f2abf375f7d7104fa01b8e97c9411b8bbd
Jesus Avelar <wisusdev@gmail.com>
2024-11-14 23:56:11 -0600

    Añade soporte para el servicio Chocolatey en WindowsService, incluyendo su información y acciones.

--- lib/app/platforms/windows/services.dart
+++ lib/app/platforms/windows/services.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:fullstak_tools/app/platforms/windows/apache_service.dart';
+import 'package:fullstak_tools/app/platforms/windows/chocolatey_service.dart';
 import 'package:fullstak_tools/app/platforms/windows/mysql_service.dart';
 import 'package:fullstak_tools/app/platforms/windows/php_service.dart';
 
 class WindowsService {
 
     static List<Map<String, dynamic>> servicesAvailable  = [
+        {
+            'name': 'Chcolatey',
+            'version': 'unknown',
+            'icon': Icons.ac_unit,
+            'color': Colors.grey,
+        },
         {
             'name': 'Apache',
             'version': 'unknown',
@@ -40,10 +47,15 @@ class WindowsService {
             return MySQLService.getVersion();
         }
 
-        return 'unknown';
+        return null;
     }
 
     static getActions(BuildContext context, String service) {    
+
+        if (service == 'Chcolatey') {
+            return ChocolateyService.actions(context);
+        }
+
         if (service == 'PHP') {
             return PHPService.actions(context);
         }
