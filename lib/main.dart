67be7543b7146341563144b26e37dd36e4c09749
Jesus Avelar <wisusdev@gmail.com>
2024-11-15 23:38:35 -0600

    Desactiva la bandera de modo de depuración en la aplicación Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,8 @@ class MyApp extends StatelessWidget {
   	Widget build(BuildContext context) {
 		return MaterialApp(
 	  		title: 'Flutter Demo',
+            
+            debugShowCheckedModeBanner: false,
 	  		
 			theme: ThemeData(
 				primarySwatch: Colors.blue,
