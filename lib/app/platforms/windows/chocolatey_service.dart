101e36bec842abf5ce8efbe3f145dc7277e6173d
Jesus Avelar <wisusdev@gmail.com>
2024-11-15 23:36:55 -0600

    Añade botones de instalación y desinstalación en ChocolateyService, y un enlace al sitio de Chocolatey. Implementa la lógica para desinstalar Chocolatey y limpiar variables de entorno.

--- lib/app/platforms/windows/chocolatey_service.dart
+++ lib/app/platforms/windows/chocolatey_service.dart
@@ -1,23 +1,62 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:fullstak_tools/app/shared/app_logs.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ChocolateyService {
 
     static List<Widget> actions(BuildContext context) {
-        return [
-            IconButton(
-                icon: const Icon(Icons.download),
-                color: Colors.white,
-                onPressed: () {
-                    install();
-                },
-            )
-        ];
+
+        List<Widget> actions = [];
+
+        Widget installButton = IconButton(
+            icon: const Icon(Icons.download),
+            color: Colors.white,
+            onPressed: () {
+                install();
+            },
+        );
+
+        Widget uninstallButton = IconButton(
+            icon: const Icon(Icons.delete),
+            color: Colors.white,
+            onPressed: () {
+                uninstall();
+            },
+        );
+
+        Widget siteLink = IconButton(
+            icon: const Icon(Icons.link),
+            color: Colors.white,
+            onPressed: () async {
+                final Uri url = Uri.parse('https://chocolatey.org');
+                
+                if(!await launchUrl(url, mode: LaunchMode.inAppBrowserView)) {
+                    throw 'Could not launch $url';
+                }
+            },
+        );
+
+        if (getVersion() == '') {
+            actions.add(installButton);
+        } else {
+            actions.add(uninstallButton);
+        }
+
+        actions.add(siteLink);
+        
+        return actions;
     }
 
     static Future<String> getVersion() async {
-        var process = await Process.start('choco', ['--version'], runInShell: true);
+
+        // Validate if folder exists
+        if (!await Directory('C:\\ProgramData\\chocolatey').exists()) {
+            return '';
+        }
+        
+        var process = await Process.start('C:\\ProgramData\\chocolatey\\bin\\choco.exe', ['--version'], runInShell: true);
 
         await process.stdin.close();
 
@@ -31,7 +70,7 @@ class ChocolateyService {
         }
 
         if (error.isNotEmpty) {
-            print('Error: $error');
+            Log().write('Error: $error', 'ERROR');
             return '';
         }
 
@@ -40,7 +79,7 @@ class ChocolateyService {
 
     static Future<String> install() async {
 
-        print('Instalando Chocolatey...');
+        Log().write('powershell -Command "Start-Process powershell -ArgumentList \"Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\'))\" -Verb runAs"', 'INFO');
 
         var process = await Process.start(
             'powershell',
@@ -62,11 +101,47 @@ class ChocolateyService {
         var error = await process.stderr.transform(const SystemEncoding().decoder).join();
 
         if (error.isNotEmpty) {
-            print('Error: $error');
+            Log().write('Error: $error', 'ERROR');
         }
 
-        print(output);
-
         return output;
+    }
+
+    static Future<String> uninstall() async {
+
+        // Delete Chocolatey folder
+        var chocolateyFolder = Directory(r'C:\ProgramData\chocolatey');
+        if (await chocolateyFolder.exists()) {
+            await chocolateyFolder.delete(recursive: true);
+            Log().write('Deleted Chocolatey folder', 'INFO');
+        } else {
+            Log().write('Chocolatey folder not found', 'INFO');
+        }
+
+        // Remove environment variables
+        var envVars = [
+            'ChocolateyInstall',
+            'ChocolateyToolsLocation',
+            'ChocolateyLastPathUpdate'
+        ];
+
+        for (var envVar in envVars) {
+            await Process.run('powershell', ['-Command', 'Remove-ItemProperty', '-Path', 'HKCU:\\Environment', '-Name', envVar], runInShell: true);
+            await Process.run('powershell', ['-Command', 'Remove-ItemProperty', '-Path', 'HKLM:\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment', '-Name', envVar], runInShell: true);
+            Log().write('Removed environment variable: $envVar', 'INFO');
+        }
+
+        // Update PATH variable
+        var path = Platform.environment['PATH'];
+        if (path != null) {
+            var newPath = path.split(';').where((p) => !p.contains('chocolatey')).join(';');
+            await Process.run('powershell', ['-Command', '[System.Environment]::SetEnvironmentVariable("PATH", "$newPath", "User")'], runInShell: true);
+            await Process.run('powershell', ['-Command', '[System.Environment]::SetEnvironmentVariable("PATH", "$newPath", "Machine")'], runInShell: true);
+            Log().write('Updated PATH variable', 'INFO');
+        }
+
+        Log().write('Chocolatey uninstalled successfully', 'INFO');
+
+        return '';
     }
 }
