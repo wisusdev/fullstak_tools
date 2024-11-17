4c804bcaeb42d3ddc5b8736291b0e4b6f0f4ba07
Jesus Avelar <wisusdev@gmail.com>
2024-11-17 12:32:33 -0600

    Refactor ChocolateyService to improve async handling and logging; add retry logic for folder deletion

--- lib/app/platforms/windows/chocolatey_service.dart
+++ lib/app/platforms/windows/chocolatey_service.dart
@@ -5,24 +5,25 @@ import 'package:fullstak_tools/app/shared/app_logs.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ChocolateyService {
-
-    static List<Widget> actions(BuildContext context) {
-
+    
+    static Future<List<Widget>> actions(BuildContext context, Function(String) updateServiceActions) async {
         List<Widget> actions = [];
 
         Widget installButton = IconButton(
             icon: const Icon(Icons.download),
             color: Colors.white,
-            onPressed: () {
-                install();
+            onPressed: () async {
+                await install();
+                updateServiceActions('Chcolatey');
             },
         );
 
         Widget uninstallButton = IconButton(
             icon: const Icon(Icons.delete),
             color: Colors.white,
-            onPressed: () {
-                uninstall();
+            onPressed: () async {
+                await uninstall();
+                updateServiceActions('Chcolatey');
             },
         );
 
@@ -31,21 +32,20 @@ class ChocolateyService {
             color: Colors.white,
             onPressed: () async {
                 final Uri url = Uri.parse('https://community.chocolatey.org/packages');
-                
-                if(!await launchUrl(url, mode: LaunchMode.inAppBrowserView)) {
+                if (!await launchUrl(url, mode: LaunchMode.inAppBrowserView)) {
                     throw 'Could not launch $url';
                 }
             },
         );
 
-        if (getVersion() == '') {
+        String version = await getVersion();
+        if (version == '') {
             actions.add(installButton);
         } else {
             actions.add(uninstallButton);
         }
 
         actions.add(siteLink);
-        
         return actions;
     }
 
@@ -78,8 +78,7 @@ class ChocolateyService {
     }
 
     static Future<String> install() async {
-
-        Log().write('powershell -Command "Start-Process powershell -ArgumentList \"Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\'))\" -Verb runAs"', 'INFO');
+        Log().write('Installing Chocolatey', 'INFO');
 
         var process = await Process.start(
             'powershell',
@@ -89,7 +88,7 @@ class ChocolateyService {
                 'powershell',
                 '-ArgumentList',
                 '"Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\'))"',
-                '-Verb', // -Verb runAs is used to run the process as administrator
+                '-Verb',
                 'runAs'
             ],
             runInShell: true,
@@ -102,20 +101,39 @@ class ChocolateyService {
 
         if (error.isNotEmpty) {
             Log().write('Error: $error', 'ERROR');
+        } else {
+            Log().write('Chocolatey installed successfully: $output', 'INFO');
         }
 
         return output;
     }
 
     static Future<String> uninstall() async {
-        Log().write('Uninstalling Chocolatey', 'INFO');
+        Log().write('Uninstalling Chocolatey', 'WARNING');
+        String output = await deleteChocolateyFolder();
 
-        // Delete Chocolatey folder
+        // Verificar si la carpeta aún existe y volver a intentar eliminarla
+        await Future.delayed(const Duration(seconds: 2)); // Esperar 2 segundos
+        
+        if (await Directory('C:\\ProgramData\\chocolatey').exists()) {
+            Log().write('Retrying to delete Chocolatey folder', 'WARNING');
+            output = await deleteChocolateyFolder();
+        }
+
+        return output;
+    }
+
+    static Future<String> deleteChocolateyFolder() async {
         var process = await Process.start(
             'powershell',
             [
                 '-Command',
-                'Start-Process powershell -ArgumentList "Remove-Item -Path \'C:\\ProgramData\\chocolatey\' -Recurse -Force" -Verb runAs'
+                'Start-Process',
+                'powershell',
+                '-ArgumentList',
+                '"Remove-Item -Path \'C:\\ProgramData\\chocolatey\' -Recurse -Force"',
+                '-Verb',
+                'runAs'
             ],
             runInShell: true,
         );
@@ -127,10 +145,10 @@ class ChocolateyService {
 
         if (error.isNotEmpty) {
             Log().write('Error: $error', 'ERROR');
+        } else {
+            Log().write('Chocolatey folder deleted successfully', 'INFO');
         }
 
-        Log().write('Chocolatey uninstalled successfully: $output', 'INFO');
-
-        return '';
+        return output;
     }
 }
