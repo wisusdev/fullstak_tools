ee1922f42e2a55b99bb3541b51c46e175718a3fc
Jesus Avelar <wisusdev@gmail.com>
2024-11-22 22:43:46 -0600

    Refactor WindowsService para utilizar instancia de PHPService en lugar de llamadas estáticas y eliminar referencia a Chocolatey.

--- lib/app/platforms/windows/services.dart
+++ lib/app/platforms/windows/services.dart
@@ -6,13 +6,9 @@ import 'package:fullstak_tools/app/platforms/windows/php_service.dart';
 
 class WindowsService {
 
+    final PHPService _phpService = PHPService();
+
     static List<Map<String, dynamic>> servicesAvailable  = [
-        {
-            'name': 'Chcolatey',
-            'version': 'unknown',
-            'icon': Icons.cloud_download,
-            'color': Colors.grey,
-        },
         {
             'name': 'PHP',
             'version': 'unknown',
@@ -21,14 +17,14 @@ class WindowsService {
         },
     ];
 
-    static getVersion(String service) {
+    getVersion(String service) {
 
         if (service == 'Chcolatey') {
             return ChocolateyService.getVersion();
         }
 
         if (service == 'PHP') {
-            return PHPService.getVersion();
+            return _phpService.getVersion();
         }
 
         if (service == 'Apache') {
@@ -42,14 +38,14 @@ class WindowsService {
         return null;
     }
 
-    static getActions(BuildContext context, String service, Function(String) updateServiceActions) {    
+    getActions(BuildContext context, String service, Function(String) updateServiceActions) {    
 
         if (service == 'Chcolatey') {
             return ChocolateyService.actions(context, updateServiceActions);
         }
 
         if (service == 'PHP') {
-            return PHPService.actions(context, updateServiceActions);
+            return _phpService.actions(context, updateServiceActions);
         }
 
         return <Widget>[];
