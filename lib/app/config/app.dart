69528ab8018cdf3c42074f2f234ed94b3e69c685
Jesus Avelar <wisusdev@gmail.com>
2024-11-22 22:42:39 -0600

    Añade archivo de configuración para la aplicación: define nombre, versión, descripción, autor, email, idioma y ruta de instalación en Windows.

---
+++ lib/app/config/app.dart
@@ -0,0 +1,10 @@
+// windows install path
+const appName = 'avegonia';
+const appVersion = '1.0.0';
+const appDescription = 'Avegonia es un software de gestión de proyectos de software';
+const appAuthor = 'Jesus Avelar';
+const appEmail = 'wisusdev@gmail.com';
+
+const appLang = 'es';
+
+const windowsInstallPath = 'C:\\avegonia\\bin';
