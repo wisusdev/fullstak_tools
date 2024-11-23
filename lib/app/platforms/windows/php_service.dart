83d5000204bb2e7d08456a4a2c85834d00113164
Jesus Avelar <wisusdev@gmail.com>
2024-11-22 22:43:35 -0600

    Actualiza PHPService para instalar y desinstalar PHP usando PowerShell, incluyendo verificación de instalación y manejo de errores.

--- lib/app/platforms/windows/php_service.dart
+++ lib/app/platforms/windows/php_service.dart
@@ -1,11 +1,18 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:fullstak_tools/app/config/app.dart';
 import 'package:fullstak_tools/app/shared/app_logs.dart';
 
 class PHPService {
 
-    static Future<List<Widget>> actions(BuildContext context, Function(String) updateServiceActions) async {
+    var windowsPath = '$windowsInstallPath\\php';
+
+    var phpVersion = 'php-8.4.1-Win32-vs17-x64';
+    var phpFileDownloadExtension = 'zip';
+    var phpDownloadnUrl = 'https://windows.php.net/downloads/releases';
+
+    Future<List<Widget>> actions(BuildContext context, Function(String) updateServiceActions) async {
 
         List<Widget> actions = [];
 
@@ -38,8 +45,12 @@ class PHPService {
         return actions;
     }
 
-    static Future<String> getVersion() async {
-        var process = await Process.start('php', ['--version'], runInShell: true);
+    Future<String> getVersion() async {
+        if (!await Directory('$windowsPath\\$phpVersion').exists()) {
+            return '';
+        }
+
+        var process = await Process.start('$windowsPath\\$phpVersion\\php.exe', ['--version'], runInShell: true);
 
         await process.stdin.close();
 
@@ -60,11 +71,12 @@ class PHPService {
         return output;
     }
 
-    static Future<void> install() async {
+    Future<String> install() async {
 
-        Log().write('Instalando PHP...', 'INFO');
-
-        var process = await Process.start('choco', ['install', 'php'], runInShell: true);
+        var process = await Process.start('powershell', [
+            '-Command',
+            'Invoke-WebRequest -Uri "$phpDownloadnUrl/$phpVersion.$phpFileDownloadExtension" -OutFile "$phpVersion.$phpFileDownloadExtension"; Expand-Archive -Path "$phpVersion.$phpFileDownloadExtension" -DestinationPath "$windowsPath\\$phpVersion"; if (Test-Path "$windowsPath\\$phpVersion.$phpFileDownloadExtension") { Remove-Item -Path "$windowsPath\\$phpVersion.$phpFileDownloadExtension"; }'
+        ], runInShell: true);
 
         await process.stdin.close();
 
@@ -75,14 +87,41 @@ class PHPService {
             Log().write('Error: $error', 'ERROR');
         }
 
-        Log().write('PHP instalado - $output', 'INFO');
+        // Espera unos segundos adicionales para asegurarse de que la carpeta se haya creado
+        await Future.delayed(const Duration(seconds: 10));
+
+        // verificamos si la carpeta se creo cada 2 segundos, pero finalizamos despues de 60 segundos
+        int elapsedSeconds = 0;
+        const int checkInterval = 2;
+        const int timeout = 60;
+
+        while (elapsedSeconds < timeout) {
+            if (await Directory('$windowsPath\\$phpVersion').exists()) {
+                Log().write('PHP instalado - $output', 'INFO');
+                break;
+            }
+
+            await Future.delayed(const Duration(seconds: checkInterval));
+            elapsedSeconds += checkInterval;
+        }
+
+        if (elapsedSeconds >= timeout) {
+            Log().write('Error al instalar PHP: No se encontró la carpeta de instalación', 'ERROR');
+        }
+
+        return output;
     }
 
-    static Future<void> uninstall() async {
+    Future<String> uninstall() async {
+        if (!await Directory('$windowsPath\\$phpVersion').exists()) {
+            Log().write('Error: El directorio de PHP no existe', 'ERROR');
+            return 'Error: El directorio de PHP no existe';
+        }
 
-        Log().write('Desinstalando PHP...', 'INFO');
-
-        var process = await Process.start('choco', ['uninstall', 'php'], runInShell: true);
+        var process = await Process.start('powershell', [
+            '-Command',
+            'Remove-Item -Recurse -Force "$windowsPath\\$phpVersion"'
+        ], runInShell: true);
 
         await process.stdin.close();
 
@@ -91,8 +130,10 @@ class PHPService {
 
         if (error.isNotEmpty) {
             Log().write('Error: $error', 'ERROR');
+        } else {
+            Log().write('PHP desinstalado', 'INFO');
         }
 
-        Log().write('PHP desinstalado - $output', 'INFO');
+        return output;
     }
 }
